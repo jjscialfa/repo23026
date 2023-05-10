21eb696667e547ca2b9797fae8b24ccf373da9d9
jjscialfa <jjscialfa@gmail.com>
2023-05-10 18:36:22 -0300

    Miércoles 10-05-2023 rediseño del sitio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let cadFooter=`
+
+        <nav>
+            <a href="https://api.whatsapp.com/send?phone=3416224513" target="_blank">
+                <img width="30" src="./img/whatsapp.png" alt="logo whatsapp">
+            </a>
+
+            <a href="https://www.facebook.com/tusmuebles.ar/" target="_blank">
+                <img width="30" src="./img/facebook.png" alt="logo facebook">
+            </a>
+
+            <a href="https://www.instagram.com/" target="_blank">
+                <img width="30" src="./img/instagram.png" alt="logo instagram">
+            </a>
+
+            <a href="mailto:jjscialfa@gmail.com" target="_blank">
+                <img width="30" src="/img/correo-electronico-vacio.png" alt="logo mail">
+            </a>
+
+        </nav>
+        <p>Sitio desarrollado en Codo a Codo - Equipo 19</p>
+`
+document.querySelector("footer").innerHTML=cadFooter
